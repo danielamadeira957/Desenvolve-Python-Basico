53d5d770622e932a1e3bcdc3ec194118df501c50
danielamadeira957 <danibatista143@gmail.com>
2026-02-13 16:09:25 -0300

    Add files via upload

---
+++ Módulo 2 python/Tipos de dados/aula2-questao2.py
@@ -0,0 +1,10 @@
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+
+soma_valores = valor1 + valor2
+
+print(texto, type(texto))
+print(valor1, type(valor1))
+print(valor2, type(valor2))
+print(soma_valores, type(soma_valores))
