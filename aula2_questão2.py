9d54a8ddb1a65f15f6ece726808dd7dc20e065b7
danielamadeira957 <danibatista143@gmail.com>
2026-03-01 20:57:28 -0300

    Delete aula2_questão2.py

--- aula2_questão2.py
+++
@@ -1,4 +0,0 @@
-dade_juliana = int(input("digite a idade de juliana:"))
-idade_cris = int (input("digite a idade do cris:"))
-pode_entrar = idade_juliana > 17 or idade_cris > 17
-print (pode_entrar)
