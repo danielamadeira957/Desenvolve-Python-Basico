9a6bafb7617db32845a767c18fa46a9028c3b55b
danielamadeira957 <danibatista143@gmail.com>
2026-05-21 14:34:02 -0300

    Delete Módulo 6 python directory

--- Módulo 6 python/Operações e Funções/Compreensão/aula4_questão4.py
+++
@@ -1,7 +0,0 @@
-alunos = ["Maria", "Jose", "Carla", "Sol"]
-notas = [35, 50, 20, 80]
-
-#reescrita com compreensão de listas
-aprovados = [alunos[i] for i in range (len(notas)) if notas [i] >= 60]
-
-print (aprovados)
